6951741796126b3adaf8339dc118a43576921a87
Terrysyoon <yoonsb@hanyang.ac.kr>
2023-01-19 19:55:45 +0900

    FIX: SensorHub.c
    Fix a function name that is not written wrong and typo.

--- 0_Src/AppSw/Tricore/SDP/SensorHub/SensorHub.c
+++ 0_Src/AppSw/Tricore/SDP/SensorHub/SensorHub.c
@@ -66,33 +66,33 @@ void SDP_SensorHub_init(void)
 
 }
 
-void SDP_ShockValue_run_1ms(void)
+void SDP_SensorHub_run_1ms(void)
 {
     /* CAN message refresh*/
     
     /*FRONT~*/
     if(CanCommunication_receiveMessage(&SensorHub_FRONT.SensorHub_msgADC))
     {
-    	SensorHub_FRONT.SensorHub_msgADC.RecievedData[0]      =   SensorHub_FRONT.SensorHubADC.msg.data[0];
-    	SensorHub_FRONT.SensorHub_msgADC.RecievedData[1]      =   SensorHub_FRONT.SensorHubADC.msg.data[1];
+    	SensorHub_FRONT.SensorHub_msgADC.ReceivedData[0]      =   SensorHub_FRONT.SensorHubADC.msg.data[0];
+    	SensorHub_FRONT.SensorHub_msgADC.ReceivedData[1]      =   SensorHub_FRONT.SensorHubADC.msg.data[1];
     }
     if(CanCommunication_receiveMessage(&SensorHub_FRONT.SensorHub_msgWSS))
     {
-        SensorHub_FRONT.SensorHub_msgWSS.RecievedData[0]      =   SensorHub_FRONT.SensorHubWSS.msg.data[0];
-        SensorHub_FRONT.SensorHub_msgWSS.RecievedData[1]      =   SensorHub_FRONT.SensorHubWSS.msg.data[1];
+        SensorHub_FRONT.SensorHub_msgWSS.ReceivedData[0]      =   SensorHub_FRONT.SensorHubWSS.msg.data[0];
+        SensorHub_FRONT.SensorHub_msgWSS.ReceivedData[1]      =   SensorHub_FRONT.SensorHubWSS.msg.data[1];
     }
     /*~FRONT*/
 
     /*REAR~*/
     if(CanCommunication_receiveMessage(&SensorHub_REAR.SensorHub_msgADC))
     {
-    	SensorHub_REAR.SensorHub_msgADC.RecievedData[0]      =   SensorHub_REAR.SensorHubADC.msg.data[0];
-    	SensorHub_REAR.SensorHub_msgADC.RecievedData[1]      =   SensorHub_REAR.SensorHubADC.msg.data[1];
+    	SensorHub_REAR.SensorHub_msgADC.ReceivedData[0]      =   SensorHub_REAR.SensorHubADC.msg.data[0];
+    	SensorHub_REAR.SensorHub_msgADC.ReceivedData[1]      =   SensorHub_REAR.SensorHubADC.msg.data[1];
     }
     if(CanCommunication_receiveMessage(&SensorHub_REAR.SensorHub_msgWSS))
     {
-        SensorHub_REAR.SensorHub_msgWSS.RecievedData[0]      =   SensorHub_REAR.SensorHubWSS.msg.data[0];
-        SensorHub_REAR.SensorHub_msgWSS.RecievedData[1]      =   SensorHub_REAR.SensorHubWSS.msg.data[1];
+        SensorHub_REAR.SensorHub_msgWSS.ReceivedData[0]      =   SensorHub_REAR.SensorHubWSS.msg.data[0];
+        SensorHub_REAR.SensorHub_msgWSS.ReceivedData[1]      =   SensorHub_REAR.SensorHubWSS.msg.data[1];
     }
     /*~REAR*/
 
